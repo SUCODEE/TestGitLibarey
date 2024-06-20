5997e139ac0ab8c48c70f8a7a8545e5e48a8fe2b
sucodee <sucodeins@gmail.com>
2024-06-20 14:34:38 +0200

    Initial Commit

---
+++ TestGitLibarey/TestGitLibareyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitLibareyApp.swift
+//  TestGitLibarey
+//
+//  Created by  yourexz on 2024-06-20.
+//
+
+import SwiftUI
+
+@main
+struct TestGitLibareyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
